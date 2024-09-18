50f0c39262bb9b22d91dc3727b8965b04e69df42
moustapha96 <68778997+moustapha96@users.noreply.github.com>
2024-09-18 16:38:57 +0000

    add 4 image

--- models/product_template.py
+++ models/product_template.py
@@ -6,3 +6,4 @@ class ProductTemplate(models.Model):
     image_1 = fields.Binary(string='Image 1', attachment=True)
     image_2 = fields.Binary(string='Image 2', attachment=True)
     image_3 = fields.Binary(string='Image 3', attachment=True)
+    image_4 = fields.Binary(string='Image 4', attachment=True)
