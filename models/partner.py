1ef8db9f806d0730f6c012cd4385eb9b4f315b9c
moustapha96 <68778997+moustapha96@users.noreply.github.com>
2024-10-07 15:25:43 +0000

    update

--- models/partner.py
+++ models/partner.py
@@ -3,10 +3,10 @@ from odoo import  fields, models, _
 class Partner(models.Model):
     _inherit = 'res.partner'
 
-    # password = fields.Char(string='Mot de passe de connexion sur la partie web',widget='password', required=False)
-    # is_verified = fields.Boolean(string='Etat verification compte mail', default=False)
-    # avatar = fields.Char(string='Photo profil Client', required=False)
-    # role = fields.Selection([
-    #     ('main_user', 'Utilisateur Principal'),
-    #     ('secondary_user', 'Utilisateur Secondaire')
-    # ], string='Rôle', default='secondary_user')
+    password = fields.Char(string='Mot de passe de connexion sur la partie web',widget='password', required=False)
+    is_verified = fields.Boolean(string='Etat verification compte mail', default=False)
+    avatar = fields.Char(string='Photo profil Client', required=False)
+    role = fields.Selection([
+        ('main_user', 'Utilisateur Principal'),
+        ('secondary_user', 'Utilisateur Secondaire')
+    ], string='Rôle', default='secondary_user')
