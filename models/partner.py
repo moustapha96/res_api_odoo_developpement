9930e0994f21fe337c3b54e4913d227983ff753a
moustapha96 <68778997+moustapha96@users.noreply.github.com>
2024-10-07 15:24:04 +0000

    upd

--- models/partner.py
+++ models/partner.py
@@ -4,7 +4,7 @@ class Partner(models.Model):
     _inherit = 'res.partner'
 
     # password = fields.Char(string='Mot de passe de connexion sur la partie web',widget='password', required=False)
-    is_verified = fields.Boolean(string='Etat verification compte mail', default=False)
+    # is_verified = fields.Boolean(string='Etat verification compte mail', default=False)
     avatar = fields.Char(string='Photo profil Client', required=False)
     role = fields.Selection([
         ('main_user', 'Utilisateur Principal'),
