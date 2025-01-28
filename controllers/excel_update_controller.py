3116d5ed63476e9193369cb8b70c86b11b353dd3
Al hussein khouma <alhusseinkhouma0@gmail.com>
2025-01-28 15:27:12 +0000

    create crm model

--- controllers/excel_update_controller.py
+++ controllers/excel_update_controller.py
@@ -10,128 +10,124 @@ import werkzeug
 
 
 class CRMUpdateController(http.Controller):
-    @http.route('/api/create_or_update_crm1', type='http',  auth='none', methods=['POST'], cors="*", csrf=False)
-    def create_or_update_crm1(self, **post):
-        try:
-
-            if not request.env.user or request.env.user._is_public():
-                admin_user = request.env.ref('base.user_admin')
-                request.env = request.env(user=admin_user.id)
-
-            data = json.loads(request.httprequest.data)
-            crm_data = data.get('data')
-
-            if not crm_data:
-                return json.dumps({"status": "error", "message": "Aucune donnée CRM reçue"})
-
-            CRM = request.env['crm.lead'].sudo()
-            Partner = request.env['res.partner'].sudo()
-
-         
-            existing_crm = CRM.search([('email_from', '=', crm_data.get('email'))], limit=1)
 
 
-            existing_crm = CRM.search([
-                '|',
-                ('email_from', '=', crm_data.get('email')),
-                ('email_from', '=', crm_data.get('guest_id'))
-            ], limit=1)
+    # @http.route('/api/create_or_update_crm', type='http', auth='none', methods=['POST'], cors="*", csrf=False)
+    # def create_or_update_crm(self, **post):
+    #     try:
+    #         self._authenticate_user()
+    #         crm_data = self._get_crm_data()
+            
+    #         if not crm_data:
+    #             return self._json_response("error", "Aucune donnée CRM reçue", status=400)
 
+    #         CRM = request.env['crm.lead'].sudo()
+    #         Partner = request.env['res.partner'].sudo()
 
-            partner = None
-            if crm_data.get('email') != "Guest":
-                partner = Partner.search([('email', '=', crm_data.get('email'))], limit=1)
+    #         existing_crm = self._find_existing_crm(CRM, crm_data)
+    #         partner = self._find_or_create_partner(Partner, crm_data)
 
+    #         if existing_crm:
+    #             crm_id = self._update_existing_crm(existing_crm, crm_data)
+    #             message = "Tous les produits ont été ajoutés au CRM existant."
+    #         else:
+    #             crm_id = self._create_new_crm(CRM, partner, crm_data)
+    #             message = "Nouveau CRM créé avec succès."
 
+    #         return self._json_response("success", message, crm_id=crm_id)
 
-            montant_esperer = 0
-            # crm_values = {
-            #     'name': f"Opportunité pour {crm_data.get('name')}",
-            #     'partner_id': partner.id if partner else None,
-            #     'email_from': crm_data.get('email') if partner else crm_data.get('guest_id'),
-            #     'phone': crm_data.get('phone'),
-            #     'description': (
-            #         f"**Type :** {crm_data.get('type')}\n"
-            #         f"**Date :** {crm_data.get('date')}\n"
-            #         f"**Localisation :** {crm_data.get('location')}\n"
-            #         f"**Coordonnées :** {crm_data.get('localisation')}\n\n"
-            #         f"**Produits :**\n"
-            #         + "\n".join(
-            #             f"- **{produit['nom']}**\n"
-            #             f"  - Quantité : {produit['quantité']}\n"
-            #             f"  - Prix unitaire : {produit['prix']} F CFA\n"
-            #             f"  - Date : {produit['date']}\n"
-            #             f"  - Panier : {produit['panier']}\n"
-            #             for produit in crm_data.get('produits', [])
-            #         )
-            #     ),
-            #     'type': 'opportunity',
-            #     'expected_revenue': 0
-            # }
-            crm_values = {
-                'name': f"Opportunité pour {crm_data.get('name')}",
-                'partner_id': partner.id if partner else None,
-                'email_from': crm_data.get('email') if partner else crm_data.get('guest_id'),
-                'phone': crm_data.get('phone'),
-                'description': (
-                    f"**Type :** {crm_data.get('type')}\n"
-                    f"**Date :** {crm_data.get('date')}\n"
-                    f"**Localisation :** {crm_data.get('location')}\n"
-                    f"**Coordonnées :** {crm_data.get('localisation')}\n\n"
-                ),
-                'type': 'opportunity',
-                'expected_revenue': 0
-            }
+    #     except Exception as e:
+    #         return self._json_response("error", str(e), status=400)
 
-            if crm_data.get('produits'):
-                produits_description = "\n**Produits :**\n"
-                for produit in crm_data['produits']:
-                    produits_description += (
-                        f"- **{produit['nom']}**\n"
-                        f"  - Quantité : {produit['quantité']}\n"
-                        f"  - Prix unitaire : {produit['prix']} F CFA\n"
-                        f"  - Date : {produit['date']}\n"
-                        f"  - Panier : {produit['panier']}\n"
-                    )
-                    montant_esperer += produit['prix'] * produit['quantité']
-                
-                crm_values['description'] += produits_description
-                crm_values['expected_revenue'] = montant_esperer
+    # def _authenticate_user(self):
+    #     if not request.env.user or request.env.user._is_public():
+    #         admin_user = request.env.ref('base.user_admin')
+    #         request.env = request.env(user=admin_user.id)
 
+    # def _get_crm_data(self):
+    #     data = json.loads(request.httprequest.data)
+    #     return data.get('data')
 
-            # if crm_data.get('produits'):
-            #     crm_values['description'] += "\n\nProduits:\n\n"
-            #     for produit in crm_data['produits']:
-            #         crm_values['description'] += (
-            #             f"- {produit['nom']} (Quantité: {produit['quantité']}, "
-            #             f"Prix: {produit['prix']}, Date: {produit['date']}, "
-            #             f"Panier: {produit['panier']} \n\n"
-            #         )
-            #         montant_esperer += produit['prix'] * produit['quantité']
-            #     crm_values['expected_revenue'] = montant_esperer
+    # def _find_existing_crm(self, CRM, crm_data):
+    #     return CRM.search([
+    #         '|',
+    #         ('email_from', '=', crm_data.get('email')),
+    #         ('email_from', '=', crm_data.get('guest_id'))
+    #     ], limit=1)
 
-            # Mise à jour ou création du CRM
-            if existing_crm:
-                existing_crm.write(crm_values)
-                crm_id = existing_crm.id
-                message = "CRM mis à jour avec succès"
-            else:
-                new_crm = CRM.create(crm_values)
-                crm_id = new_crm.id
-                message = "Nouveau CRM créé avec succès"
+    # def _find_or_create_partner(self, Partner, crm_data):
+    #     if crm_data.get('email') != "Guest":
+    #         return Partner.search([('email', '=', crm_data.get('email'))], limit=1)
+    #     return None
 
-            return werkzeug.wrappers.Response(
-                status=200,
-                content_type='application/json; charset=utf-8',
-                headers=[('Cache-Control', 'no-store'), ('Pragma', 'no-cache')],
-                response=json.dumps({"status": "success", "message": message, "crm_id": crm_id})
-            )
-        except Exception as e:
-            return werkzeug.wrappers.Response(
-                status=400,
-                content_type='application/json; charset=utf-8',
-                response=json.dumps({"status": "error", "message": str(e)})
-            )
+    # def _update_existing_crm(self, existing_crm, crm_data):
+    #     existing_description = existing_crm.description or ""
+    #     nouveaux_produits = self._format_products(crm_data.get('produits', []), crm_data.get('total'))
+
+    #     existing_crm.write({
+    #         'description': existing_description + nouveaux_produits,
+    #         'expected_revenue': crm_data.get('total'),
+    #         'date_maj': fields.Datetime.now()
+    #     })
+    #     return existing_crm.id
+
+    # def _create_new_crm(self, CRM, partner, crm_data):
+    #     description = self._format_crm_description(crm_data)
+    #     new_crm = CRM.create({
+    #         'name': f"Opportunité pour {crm_data.get('name')}",
+    #         'partner_id': partner.id if partner else None,
+    #         'email_from': crm_data.get('email') if partner else crm_data.get('guest_id'),
+    #         'phone': crm_data.get('phone'),
+    #         'description': description,
+    #         'type': 'opportunity',
+    #         'expected_revenue': crm_data.get('total'),
+    #         'date_maj': fields.Datetime.now()
+    #     })
+    #     return new_crm.id
+
+    # def _format_products(self, produits, total):
+    #     formatted_products = "\n"
+    #     for produit in produits:
+    #         formatted_products += self._format_product(produit)
+    #     formatted_products += f"\n---\n**Total Panier : {total} F CFA**\n"
+    #     return formatted_products
+
+    # def _format_product(self, produit):
+    #     return (
+    #         f"- **{produit['nom']}**\n"
+    #         f"  - Quantité : {produit['quantité']}\n"
+    #         f"  - Prix unitaire : {produit['prix']} F CFA\n"
+    #         f"  - Montant : {produit['prix'] * produit['quantité']} F CFA\n"
+    #         f"  - Date : {produit['date']}\n"
+    #         f"  - Panier : {produit['panier']}\n"
+    #     )
+
+    # def _format_crm_description(self, crm_data):
+    #     header = (
+    #         f"**Type :** {crm_data.get('type')}\n"
+    #         f"**Date :** {crm_data.get('date')}\n"
+    #         f"**Localisation :** {crm_data.get('location')}\n"
+    #         f"**Coordonnées :** {crm_data.get('localisation')}\n\n"
+    #         f"**Produits :**\n"
+    #     )
+    #     products = self._format_products(crm_data.get('produits', []), crm_data.get('total'))
+    #     return header + products
+
+    # def _json_response(self, status, message, crm_id=None, status_code=200):
+    #     data = {
+    #         "status": status,
+    #         "message": message
+    #     }
+    #     if crm_id:
+    #         data["crm_id"] = crm_id
+
+    #     return werkzeug.wrappers.Response(
+    #         status=status_code,
+    #         content_type='application/json; charset=utf-8',
+    #         headers=[('Cache-Control', 'no-store'), ('Pragma', 'no-cache')],
+    #         response=json.dumps(data)
+    #     )
+    
 
 
     @http.route('/api/create_or_update_crm', type='http', auth='none', methods=['POST'], cors="*", csrf=False)
@@ -143,14 +139,30 @@ class CRMUpdateController(http.Controller):
                 request.env = request.env(user=admin_user.id)
 
             # Récupération des données
-            data = json.loads(request.httprequest.data)
-            crm_data = data.get('data')
+            crm_data = json.loads(request.httprequest.data or "{}")
+            if not crm_data:
+                return json.dumps({"status": "error", "message": "Les données sont vides ou invalides."})
+            
+    
 
+            # Vérifiez les champs requis
+            if not all(key in crm_data for key in ['total', 'type', 'date', 'location', 'produits']):
+                return json.dumps({"status": "error", "message": "Certains champs obligatoires sont manquants dans les données CRM."})
+
+            total = crm_data.get('total')
+            type = crm_data.get('type')
+            date = crm_data.get('date')
+            location = crm_data.get('location')
+            localisation = crm_data.get('localisation')
+            produits = crm_data.get('produits')
+
+            # Traitement des données
             if not crm_data:
                 return json.dumps({"status": "error", "message": "Aucune donnée CRM reçue"})
 
             CRM = request.env['crm.lead'].sudo()
             Partner = request.env['res.partner'].sudo()
+            LeadLine = request.env['crm.lead.line'].sudo()
 
             # Recherche d'un CRM existant
             existing_crm = CRM.search([
@@ -164,72 +176,47 @@ class CRMUpdateController(http.Controller):
             if crm_data.get('email') != "Guest":
                 partner = Partner.search([('email', '=', crm_data.get('email'))], limit=1)
 
-            montant_esperer = 0
+            lieu = f"Localisation : {location} - {localisation}\n"
+            les_produits = ""
+            for produit in produits:
+                les_produits += "nom : "+ produit['nom'] + ",quantité :"+ str(produit['quantité']) + ",prix : "+ str(produit['prix']) + "\n"
 
             if existing_crm:
-                # Récupérer la description existante
-                existing_description = existing_crm.description or ""
-
-                # Créer un set pour vérifier les produits déjà ajoutés
-                produits_existants = set()
-                for line in existing_description.splitlines():
-                    if line.startswith("- **"):  # Identifier les lignes des produits
-                        produit_nom = line.split("**")[1]
-                        produits_existants.add(produit_nom)
-
-                # Ajouter uniquement les nouveaux produits
-                nouveaux_produits = "\n"
-                for produit in crm_data.get('produits', []):
-                    if produit['nom'] not in produits_existants:
-                        nouveaux_produits += (
-                            f"- **{produit['nom']}**\n"
-                            f"  - Quantité : {produit['quantité']}\n"
-                            f"  - Prix unitaire : {produit['prix']} F CFA\n"
-                            f"  - Date : {produit['date']}\n"
-                            f"  - Panier : {produit['panier']}\n"
-                        )
-                        montant_esperer += produit['prix'] * produit['quantité']
-
-                # Mettre à jour uniquement si de nouveaux produits ont été ajoutés
-                if nouveaux_produits.strip():
-                    existing_crm.write({
-                        'description': existing_description + nouveaux_produits,
-                        'expected_revenue': existing_crm.expected_revenue + montant_esperer,
-                        'date_maj': fields.Datetime.now()
-                    })
-                    message = "Produits ajoutés à l'existant."
-                else:
-                    message = "Aucun nouveau produit à ajouter."
+                # Enregistrer la ligne de panier
+                LeadLine.create({
+                    'lead_id': existing_crm.id,
+                    'products': les_produits,
+                    'date': fields.Datetime.now(),
+                    'amount': total,
+                    'type': type,
+                })
+                existing_crm.write({
+                    'expected_revenue':  total,
+                    'date_maj': fields.Datetime.now(),
+                    "description": existing_crm.description + "\t " + lieu 
+                })
+                message = "Tous les produits ont été ajoutés au CRM existant."
                 crm_id = existing_crm.id
             else:
                 # Si le CRM n'existe pas, création avec tous les produits
-                description = (
-                    f"**Type :** {crm_data.get('type')}\n"
-                    f"**Date :** {crm_data.get('date')}\n"
-                    f"**Localisation :** {crm_data.get('location')}\n"
-                    f"**Coordonnées :** {crm_data.get('localisation')}\n\n"
-                    f"**Produits :**\n"
-                )
-                for produit in crm_data.get('produits', []):
-                    description += (
-                        f"- \n**{produit['nom']}**\n"
-                        f"  - Quantité : {produit['quantité']}\n"
-                        f"  - Prix unitaire : {produit['prix']} F CFA\n"
-                        f"  - Date : {produit['date']}\n"
-                        f"  - Panier : {produit['panier']}\n"
-                    )
-                    montant_esperer += produit['prix'] * produit['quantité']
-
                 new_crm = CRM.create({
                     'name': f"Opportunité pour {crm_data.get('name')}",
                     'partner_id': partner.id if partner else None,
                     'email_from': crm_data.get('email') if partner else crm_data.get('guest_id'),
                     'phone': crm_data.get('phone'),
-                    'description': description,
                     'type': 'opportunity',
-                    'expected_revenue': montant_esperer,
-                    'date_maj': fields.Datetime.now()
+                    'expected_revenue': total,
+                    'date_maj': fields.Datetime.now(),
+                    "description": lieu 
                 })
+                LeadLine.create({
+                    'lead_id': new_crm.id,
+                    'products': les_produits,
+                    'date': fields.Datetime.now(),
+                    'amount': total,
+                    'type': type,
+                })
+            
                 message = "Nouveau CRM créé avec succès."
                 crm_id = new_crm.id
 
--- models/crm_model.py
+++ models/crm_model.py
@@ -4,12 +4,22 @@ from odoo import api, fields, models, _
 class CrmLead(models.Model):
     _inherit = 'crm.lead'
 
-
-    # date_maj = fields.Datetime(string="Date Mise à jour", default=fields.Datetime.now, copy=False, readonly=False)
-
     date_maj = fields.Datetime(string="Date de mise à jour", default=fields.Datetime.now, help="La date de dernière mise à jour")
+    lead_line_ids = fields.One2many('crm.lead.line', 'lead_id', string='Lead Lines')
     
-    # @api.model
-    # def write(self, vals):
-    #     vals['date_maj'] = fields.Datetime.now()
-    #     return super(CrmLead, self).write(vals)
+
+
+
+class CrmLeadLine(models.Model):
+    _name = 'crm.lead.line'
+    _description = 'CRM Lead Line'
+
+    lead_id = fields.Many2one('crm.lead', string='Lead', required=True, ondelete='cascade')
+    products = fields.Text(string='Products', required=False)
+    date = fields.Datetime(string="Date du panier", default=fields.Datetime.now, help="La date de création du panier")
+    amount = fields.Float(string='Amount', required=True)
+    type = fields.Selection([
+        ('commande', 'Commande'),
+        ('precommande', 'Précommande'),
+        ('acredit', 'A crédit'),
+    ], string='Type', required=True , default='commande')
