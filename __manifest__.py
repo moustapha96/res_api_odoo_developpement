2c49d78edf4e43a1b430d60d0742f74fdad18697
Al hussein khouma <alhusseinkhouma0@gmail.com>
2025-02-07 15:29:17 +0000

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -36,7 +36,7 @@
         'views/res_partner_view.xml',
         'views/ir_cron_data.xml',
         'views/crm_lead_view.xml',
-        'views/pack_views.xml',
+        # 'views/pack_views.xml',
     ],
     'images': [
         'static/description/banner.png',
