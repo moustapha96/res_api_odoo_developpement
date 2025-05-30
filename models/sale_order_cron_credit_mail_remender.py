bed067886584684c4695402299894c8d63ac2b3c
Al hussein khouma <alhusseinkhouma0@gmail.com>
2025-05-30 14:43:10 +0000

    update url ccbm

--- models/sale_order_cron_credit_mail_remender.py
+++ models/sale_order_cron_credit_mail_remender.py
@@ -185,7 +185,7 @@ class SaleOrderCronCreditMailReminder(models.Model):
                                     <table border="0" cellpadding="0" cellspacing="0" width="590" style="min-width: 590px; background-color: #F1F1F1; color: #454748; padding: 8px; border-collapse:separate;">
                                         <tr>
                                             <td style="text-align: center; font-size: 13px;">
-                                                Généré par <a target="_blank" href="https://ccbme.sn" style="color: #875A7B;">CCBM SHOP</a>
+                                                Généré par <a target="_blank" href="https://www.ccbmshop.sn" style="color: #875A7B;">CCBM SHOP</a>
                                             </td>
                                         </tr>
                                     </table>
