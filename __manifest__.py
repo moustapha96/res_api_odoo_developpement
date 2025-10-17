6a68b1c738ba523e6753a8a83776135b0421b6c2
Al hussein Khouma/Full Stack Developper <68778997+moustapha96@users.noreply.github.com>
2025-10-17 16:39:08 +0000

    desactivation

--- __manifest__.py
+++ __manifest__.py
@@ -35,7 +35,7 @@
         
         'views/ir_model_view.xml',
         'views/res_partner_view.xml',
-        'views/product_template_view.xml',
+        # 'views/product_template_view.xml',
         
         'views/ir_cron_data.xml',
         'views/crm_lead_view.xml',
